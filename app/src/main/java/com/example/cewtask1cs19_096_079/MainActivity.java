a4736daad97a27cb36db508598909f9af7e6e953
Sohaib-50 <i.am_sa@yahoo.com>
2021-01-15 15:32:09 +0500

    Added empty project files

---
+++ app/src/main/java/com/example/cewtask1cs19_096_079/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cewtask1cs19_096_079;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

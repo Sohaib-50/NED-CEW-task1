be107def9f9f7dcb097da358609c36299ac6b8a7
Sohaib-50 <i.am_sa@yahoo.com>
2021-01-15 19:44:17 +0500

    Final submission version

--- app/src/main/java/com/example/cewtask1cs19_096_079/MainActivity.java
+++ app/src/main/java/com/example/cewtask1cs19_096_079/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.cewtask1cs19_096_079;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,37 +20,33 @@ public class MainActivity extends AppCompatActivity {
     EditText et_email_id;
     EditText et_password;
     EditText et_confirm_password;
-    EditText[] edit_texts = {
-            et_first_name,
-            et_last_name,
-            et_gender,
-            et_date_of_birth,
-            et_email_id,
-            et_password,
-            et_confirm_password
-    };
+    ArrayList<EditText> edit_texts = new ArrayList<>();
     Button btn_signup;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        et_first_name = findViewById(R.id.et_first_name);
-        et_last_name = findViewById(R.id.et_last_name);
-        et_gender = findViewById(R.id.et_gender);
-        et_date_of_birth = findViewById(R.id.et_date_of_birth);
-        et_email_id = findViewById(R.id.et_email_id);
-        et_password = findViewById(R.id.et_password);
-        et_confirm_password = findViewById(R.id.et_confirm_password)
-        btn_signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onSignupButtonClick();
-            }
-        });
-    }
+        et_first_name = (EditText) findViewById(R.id.et_first_name);
+        et_last_name = (EditText) findViewById(R.id.et_last_name);
+        et_gender = (EditText) findViewById(R.id.et_gender);
+        et_date_of_birth = (EditText) findViewById(R.id.et_date_of_birth);
+        et_email_id = (EditText)findViewById(R.id.et_email_id);
+        et_password = (EditText) findViewById(R.id.et_password);
+        et_confirm_password = (EditText) findViewById(R.id.et_confirm_password);
+        btn_signup = (Button) findViewById(R.id.btn_signup);
 
-    private void onSignupButtonClick() {
+        edit_texts.add(et_first_name);
+        edit_texts.add(et_last_name);
+        edit_texts.add(et_gender);
+        edit_texts.add(et_date_of_birth);
+        edit_texts.add(et_email_id);
+        edit_texts.add(et_password);
+        edit_texts.add(et_confirm_password);
+
+        }
+
+    public void onSignupButtonClick(View view) {
 
         // Check if any edit text is empty
         for (EditText et: edit_texts)
@@ -90,8 +87,14 @@ public class MainActivity extends AppCompatActivity {
         String first_name = et_first_name.getText().toString();
         String last_name = et_last_name.getText().toString();
         Toast.makeText(MainActivity.this,
-                "You have successfully signed up," + first_name + " " + last_name + "!",
+                "You have successfully signed up, " + first_name + " " + last_name + "!",
                 Toast.LENGTH_LONG).show();
+
+        for (EditText et: edit_texts)
+        {
+            et.setText("");
+        }
+
 
 
 
